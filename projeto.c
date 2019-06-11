85c5c1ac4f926e5802f060ed1976c7570548736b
Lincoln <lincoln.morais@ufv.br>
2019-06-11 15:36:04 -0300

    abc

--- projeto.c
+++ projeto.c
@@ -1,1 +1,17 @@
 #include<stdio.h>
+int soma(int a,int b){
+	
+	int soma_num = a+b;
+	return soma_num;
+}
+int subtracao(int a, int b){
+	int sub_num = a-b;
+	return sub_num;
+}
+
+int main(){
+	
+	printf("soma dois numeros %d \n", soma(3,5));
+	printf("subtrai dois numeros %d \n", subtracao(7,2));
+	return 0;
+}
